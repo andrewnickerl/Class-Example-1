3c5384cdab182b9a4610583201523372175c6525
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-13 11:19:34 -0500

    Add project files.

---
+++ Class Example 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int avg = (num1 + num2 + num3) / 3;
+
+            if(avg < 10)
+            {
+                Console.WriteLine("Fail!");
+            }
+            if(avg >= 10)
+            {
+                Console.WriteLine("Pass!");
+            }
+        }
+    }
+}
